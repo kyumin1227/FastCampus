4ee7c2fef224ccdee87c9b128e3ae78cd4a9e3eb
kyumin1227 <kyumin12271227@gmail.com>
2025-02-09 19:19:27 +0900

    Lesson: ~ Part1-Ch3-03-사칙 연산 계산기

---
+++ 10ProjectBackend/Part1_OPP/src/test/java/org/example/password/WrongFixedPasswordGenerator.java
@@ -0,0 +1,9 @@
+package org.example.password;
+
+public class WrongFixedPasswordGenerator implements PasswordGenerator{
+
+    @Override
+    public String generatePassword() {
+        return "aa";
+    }
+}
