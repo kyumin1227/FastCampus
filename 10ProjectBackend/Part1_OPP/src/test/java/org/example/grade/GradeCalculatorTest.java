f9d47f3b34e1f43ddfd30634dcbb27f743925626
kyumin1227 <kyumin12271227@gmail.com>
2025-02-09 23:45:23 +0900

    Lesson: ~ Part1-Ch3-04-학점 계산기

---
+++ 10ProjectBackend/Part1_OPP/src/test/java/org/example/grade/GradeCalculatorTest.java
@@ -0,0 +1,28 @@
+package org.example.grade;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * 요구사항
+ * 평균학점 계산 방법 = (학점수 * 교과목 평점)의 합계/수강신청 총 학점 수
+ * 일급 컬렉션 사용
+ */
+public class GradeCalculatorTest {
+//    학점계산기 도메인: 이수한 과목, 학점 계산기
+
+    @DisplayName("평균 학점을 계산한다.")
+    @Test
+    void calculateGradeTest() {
+        List<Course> courses = List.of(new Course("OOP", 3, "A+"), new Course("자료구조", 3, "A+"));
+
+        GradeCalculator gradeCalculator = new GradeCalculator(courses);
+        double gradeResult = gradeCalculator.calculateGrade();
+
+        assertThat(gradeResult).isEqualTo(4.5);
+    }
+}
