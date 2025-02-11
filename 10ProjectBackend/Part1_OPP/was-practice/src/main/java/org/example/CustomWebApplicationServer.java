5865c05c5bcff9a702f142de41f4ee9f80444f42
kyumin1227 <kyumin12271227@gmail.com>
2025-02-12 00:04:37 +0900

    Lesson: ~ Part1-Ch4-03-계산기 웹 프로그램 구현하기
    - 사용자 요청이 들어올 때마다 Thread를 새로 생성하여 사용자 요청을 처리

--- 10ProjectBackend/Part1_OPP/was-practice/src/main/java/org/example/CustomWebApplicationServer.java
+++ 10ProjectBackend/Part1_OPP/was-practice/src/main/java/org/example/CustomWebApplicationServer.java
@@ -29,38 +29,9 @@ public class CustomWebApplicationServer {
                 logger.info("[CustomWebApplicationServer] client connected!");
 
                 /**
-                 * Step1 - 사용자 요청을 메인 Thread가 처리하도록 한다.
+                 * Step2 - 사용자 요청이 들어올 때마다 Thread를 새로 생성해서 사용자 요청을 처리하도록 한다.
                  */
-
-                try (InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()) {
-                    BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-                    DataOutputStream dos = new DataOutputStream(out);
-
-//                    String line;
-//                    while((line = br.readLine()) != "") {
-//                        System.out.println(line);
-//                    }
-
-                    HttpRequest httpRequest = new HttpRequest(br);
-
-//                    GET /calculate?operand1=11&operator=*&operand2=55
-                    if (httpRequest.isGetRequest() && httpRequest.matchPath("/calculate")) {
-                        QueryStrings queryStrings = httpRequest.getQueryStrings();
-
-                        int operand1 = Integer.parseInt(queryStrings.getValue("operand1"));
-                        String operator = queryStrings.getValue("operator");
-                        int operand2 = Integer.parseInt(queryStrings.getValue("operand2"));
-
-                        int result = Calculator.calculate(operand1, operator, operand2);
-                        byte[] body = String.valueOf(result).getBytes();
-
-                        HttpResponse response = new HttpResponse(dos);
-                        response.response200Header("application/json", body.length);
-                        response.responseBody(body);
-                    }
-
-                }
-
+                new Thread(new ClientRequestHandler(clientSocket)).start();
             }
 
         }
