e498d1f170841bbf86c18acc8d75115f802cd183
kyumin1227 <kyumin12271227@gmail.com>
2025-02-12 22:11:03 +0900

    Lesson: ~ Part1-Ch5-04-리팩토리 및 DB 커넥션 풀 적용
    - JDBC 코드 리팩토링
    - DB 커넥션 풀 적용

--- 10ProjectBackend/Part1_OPP/jdbc-practice/src/main/java/org/example/ConnectionManager.java
+++ 10ProjectBackend/Part1_OPP/jdbc-practice/src/main/java/org/example/ConnectionManager.java
@@ -3,15 +3,39 @@ package org.example;
 import com.zaxxer.hikari.HikariDataSource;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConnectionManager {
-    public static DataSource getDataSource() {
+
+    private static final String DB_DRIVER = "org.h2.Driver";
+    private static final String DB_URL = "jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1";
+    public static final int MAX_POOL_SIZE = 40;
+    private static final DataSource ds;
+
+    static {
         HikariDataSource hikariDataSource = new HikariDataSource();
-        hikariDataSource.setDriverClassName("org.h2.Driver");
-        hikariDataSource.setJdbcUrl("jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1");
+        hikariDataSource.setDriverClassName(DB_DRIVER);
+        hikariDataSource.setJdbcUrl(DB_URL);
         hikariDataSource.setUsername("sa");
         hikariDataSource.setPassword("");
+        hikariDataSource.setMaximumPoolSize(MAX_POOL_SIZE); // 최대 커넥션 풀 크기
+        hikariDataSource.setMinimumIdle(MAX_POOL_SIZE); // 풀에서 유휴 상태로 유지할 최소 개수
 
-        return hikariDataSource;
+        ds = hikariDataSource;
     }
+
+    public static Connection getConnection() {
+        try {
+            return ds.getConnection();
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public static DataSource getDataSource() {
+        return ds;
+    }
+
 }
